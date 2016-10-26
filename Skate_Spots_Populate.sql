d707dc2325b9972015e63edf3e0f9c72c9f11601
Adam Austin <adam.marcus.austin@gmail.com>
2016-10-26 17:57:33 -0600

    Updated populate code to work in mysql.

--- Skate_Spots_Populate.sql
+++ Skate_Spots_Populate.sql
@@ -1,21 +1,16 @@
-/*
-  Author: Adam Austin and Evan Yin
+DROP TABLE user;
 
-  A populate and build for the database for SkateSpots
-*/
+CREATE TABLE user(user_id integer PRIMARY KEY NOT NULL, user_name char(30) NOT NULL, user_first_name char(30) NOT NULL, user_password char(30) NOT NULL, user_email char(40) NOT NULL, user_rating integer);
+CREATE TABLE spot (spot_id integer primary key, x_cordinate DOUBLE(9,6), y_cordinate DOUBLE(9,6), spot_rating integer);
 
-drop table user;
-drop table spot;
-drop table tables_info;
-drop table user_spot_list;
+/*Inster Users*/
+INSERT INTO user VALUES (1, "adam.awesome", "Adam", "csci3308", "adau3486@colorado.edu", 5);
+INSERT INTO user VALUES (2, "evan" , "Evan" , "csci3308" , "evyi6056@colorado.edu" , 5);
+
+/*Insert Spots*/
+/*INSERT INTO spot VALUES (1, 40.005911, -105.263942, 5);*/
 
 
-create table user (user_id integer primary key, user_name char(30), user_first_name char(30), user_password char(30), user_email char(40), user_rating integer;
-/*Make some sort of a cordinate and photo object*/
-/*create table spot (spot_id integer primary key, x_cordinate DOUBLE(7,5), y_cordinate DOUBLE(7,5), spot_rating integer, cover_photo photo);*/
-create table tables_info (user_count integer);
+/*create table tables_info (user_count integer);*/
 /*Figure out how to make some sort of array in table)*/
 /*create table user_spot_list (user_id integer primary key, spot_id integer);*/
-
-insert into user values (1, "adam.awesome", "Adam", "csci3308", "adau3486@colorado.edu", 5);
-insert into user values (2, "evan" , "Evan" , "csci3308" , "evyi6056@colorado.edu" , 5);

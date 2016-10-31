ba71199ad121608bf54dfb824bdf1903d0870529
jackpanetta <jackpanetta@gmail.com>
2016-10-31 13:23:20 -0600

    Added Web Application

---
+++ WebApplication/SkateSpots/SkateSpots/AppCode/TestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// HttpHandler for testing the WebServer setup
+
+namespace SkateSpots
+{
+    public class TestHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("Hello!");
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
---
+++ WebApplication/SkateSpots/SkateSpots/Pages/Home.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SkateSpots.Pages.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Skate Spots!</h1>
+    
+    </div>
+    </form>
+</body>
+</html>
+
+
+

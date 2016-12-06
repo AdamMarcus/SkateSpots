03b9e8df629de0361aecebab848e8c7eb8747113
Adam Austin <adam.marcus.austin@gmail.com>
2016-12-06 11:28:15 -0700

    Functions needed to comunicate with database

--- app/src/main/java/buffsftw/skatespots/WebRequest.java
+++ app/src/main/java/buffsftw/skatespots/WebRequest.java
@@ -5,4 +5,74 @@ package buffsftw.skatespots;
  */
 
 public class WebRequest {
+    private static int userID;
+
+    public int getUserId() {
+        return userID;
+    }
+
+    public String getPassword() {
+        String password = "0";
+
+        /* Jack */
+
+        return password;
+    }
+
+    public String getName() {
+        String name = "0";
+
+        /* Jack */
+
+        return name;
+    }
+
+    public String getUserName() {
+        String userName = "0";
+
+        /* Jack */
+
+        return userName;
+    }
+
+    public int[] getSpotCollection() {
+        int[] spotCollection = new int[] { };
+
+        /* Jack */
+
+        return spotCollection;
+    }
+
+    public int getTokenCount() {
+        int tokenCount = 0;
+
+        /* Jack */
+
+        return tokenCount;
+    }
+
+
+    public void setUserId(int userID) {
+        /* Jack */
+    }
+
+    public void setPassword(String password) {
+        /* Jack */
+    }
+
+    public void setName(String name {
+        /* Jack */
+    }
+
+    public void setUserName(String userName) {
+        /* Jack */
+    }
+
+    public void addSpot() {
+        /* Jack */
+    }
+
+    public void setTokenCount() {
+        /* Jack */
+    }
 }

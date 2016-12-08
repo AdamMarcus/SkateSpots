3236e2b991f3fb51d3074bb3bdc70d58e9b03697
Adam Austin <adam.marcus.austin@gmail.com>
2016-12-07 20:53:08 -0700

    additions to populate to auto increment users and spots uppon addition

--- Database/Skate_Spots_Populate.sql
+++ Database/Skate_Spots_Populate.sql
@@ -10,16 +10,17 @@ USE skatespots
 
 GO
 CREATE TABLE users (
-  user_id integer PRIMARY KEY NOT NULL,
+  user_id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
   user_name char(30) NOT NULL,
   user_first_name char(30) NOT NULL,
   user_password char(30) NOT NULL,
   user_email char(40) NOT NULL,
-  user_rating integer
+  user_rating integer,
+  token_count integer
 );
 GO
 CREATE TABLE spots (
-  spot_id integer PRIMARY KEY NOT NULL,
+  spot_id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
   x_coordinate DECIMAL(10,6),
   y_coordinate DECIMAL(10,6),
   country char(50),
@@ -55,19 +56,20 @@ CREATE TABLE user_spot_contribution (
 );
 GO
 /* Insert users */
-INSERT INTO users VALUES (1, 'adam.awesome', 'Adam', 'csci3308', 'adau3486@colorado.edu', 5);
-INSERT INTO users VALUES (2, 'EnergeticEvan', 'Evan', 'csci3308' , 'evan.yin@colorado.edu' , 5);
-INSERT INTO users VALUES (3, 'coolcarrie', 'Carrie', 'clubsports', 'cawa1436@colorado.edu', 5);
-INSERT INTO users VALUES (4, 'FitFatin', 'Fatin', 'csci3308', 'Fatin.Kamaruzaman@colorado.edu', 5);
-INSERT INTO users VALUES (5, 'JumpingJack', 'Jack', 'csci3308', 'Jackson.Panetta@colorado.edu', 5);
-INSERT INTO users VALUES (6, 'SuperStering', 'Sterling', 'csci3308', 'Sterling.Vangeloff@colorado.edu', 5);
-INSERT INTO users VALUES (7, 'BombBlake', 'Blake', 'csci3308', 'Blake.Galbavy@colorado.edu', 5);
+INSERT INTO users (user_name, user_first_name, user_password, user_email, user_rating, token_count) VALUES ('TestUser', 'TestUserName', 'TestPassowrd', 'TestEmail@colorado.edu', 5, 50);
+INSERT INTO users (user_name, user_first_name, user_password, user_email, user_rating, token_count) VALUES ('adam.awesome', 'Adam', 'password', 'adau3486@colorado.edu', 5, 50);
+INSERT INTO users (user_name, user_first_name, user_password, user_email, user_rating, token_count) VALUES ('EnergeticEvan', 'Evan', 'csci3308' , 'evan.yin@colorado.edu' , 5, 50);
+INSERT INTO users (user_name, user_first_name, user_password, user_email, user_rating, token_count) VALUES ('coolcarrie', 'Carrie', 'clubsports', 'cawa1436@colorado.edu', 5, 50);
+INSERT INTO users (user_name, user_first_name, user_password, user_email, user_rating, token_count) VALUES ('FitFatin', 'Fatin', 'csci3308', 'Fatin.Kamaruzaman@colorado.edu', 5, 50);
+INSERT INTO users (user_name, user_first_name, user_password, user_email, user_rating, token_count) VALUES ('JumpingJack', 'Jack', 'csci3308', 'Jackson.Panetta@colorado.edu', 5, 50);
+INSERT INTO users (user_name, user_first_name, user_password, user_email, user_rating, token_count) VALUES ('SuperStering', 'Sterling', 'csci3308', 'Sterling.Vangeloff@colorado.edu', 5, 50);
+INSERT INTO users (user_name, user_first_name, user_password, user_email, user_rating, token_count) VALUES ('BombBlake', 'Blake', 'csci3308', 'Blake.Galbavy@colorado.edu', 5, 50);
 GO
 /* Insert spots */
-INSERT INTO spots VALUES (1, 40.005911, -105.263942, 'United States of America', 'Boulder', 'Colorado', 'Stair', 'Easy');
-INSERT INTO spots VALUES (2, 40.006518, -105.267539, 'United States of America', 'Boulder', 'Colorado', 'Ledge', 'Easy');
-INSERT INTO spots VALUES (3, 40.007453, -105.266391, 'United States of America', 'Boulder', 'Colorado', 'Curb', 'Medium');
-INSERT INTO spots VALUES (4, 40.004952, -105.259692, 'United States of America', 'Boulder', 'Colorado', 'Flatland', 'Easy');
+INSERT INTO spots (x_coordinate, y_coordinate, country, city, state, spot_type, spot_difficulty) VALUES (40.005911, -105.263942, 'United States of America', 'Boulder', 'Colorado', 'Stair', 'Easy');
+INSERT INTO spots (x_coordinate, y_coordinate, country, city, state, spot_type, spot_difficulty) VALUES (40.006518, -105.267539, 'United States of America', 'Boulder', 'Colorado', 'Ledge', 'Easy');
+INSERT INTO spots (x_coordinate, y_coordinate, country, city, state, spot_type, spot_difficulty) VALUES (40.007453, -105.266391, 'United States of America', 'Boulder', 'Colorado', 'Curb', 'Medium');
+INSERT INTO spots (x_coordinate, y_coordinate, country, city, state, spot_type, spot_difficulty) VALUES (40.004952, -105.259692, 'United States of America', 'Boulder', 'Colorado', 'Flatland', 'Easy');
 GO
 /* Insert spot_stats */
 INSERT INTO spot_stats VALUES (1, 3, 1, 0, 0, 1);
@@ -76,6 +78,8 @@ INSERT INTO spot_stats VALUES (3, 2, 1, 0, 0, 2);
 INSERT INTO spot_stats VALUES (4, 5, 2, 0, 0, 2);
 GO
 /* Store the spots users have in their collection */
+INSERT INTO user_spot_collection VALUES (0, 1);
+INSERT INTO user_spot_collection VALUES (0, 3);
 INSERT INTO user_spot_collection VALUES (1, 1);
 INSERT INTO user_spot_collection VALUES (1, 2);
 INSERT INTO user_spot_collection VALUES (1, 3);
